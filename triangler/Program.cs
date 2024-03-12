8f64af8ba9f99980a1217bef0bdcc20c54659502
Arsen <Cr1zzzzz@users.noreply.github.com>
2024-03-12 12:24:13 +0200

    Add project files.

---
+++ triangler/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Pyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the height of the pyramid: ");
+            int height = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < height; i++)
+            {
+                Console.WriteLine(new string(' ', height - i) + new string('*', (2 * i) + 1));
+            }
+        }
+    }
+}
